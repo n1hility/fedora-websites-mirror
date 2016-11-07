3f95f886e71af318293465119f4480004c8940dc
Ricky Elrod <ricky@elrod.me>
2016-11-07 15:17:04 -0500

    add first go of the releases JSON in a separate branch
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

---
+++ getfedora.org/build/releases-json.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import fedfind.release
+import fedfind.helpers
+import json
+
+output = []
+
+def hashify(version, milestone, arch, link, variant, subvariant):
+    return { 'version': version
+           , 'arch': arch
+           , 'link': link
+           , 'variant': variant
+           , 'subvariant': subvariant
+           }
+
+releases_to_report = [
+      fedfind.release.get_release(25, 'Beta')
+    , fedfind.release.get_release(25, 'Alpha')
+    , fedfind.release.get_release(24)
+    , fedfind.release.get_release(24, 'Beta')
+    , fedfind.release.get_release(24, 'Alpha')
+    , fedfind.release.get_release(23)
+    , fedfind.release.get_release(23, 'Beta')
+    , fedfind.release.get_release(23, 'Alpha')
+    ]
+
+for rel in releases_to_report:
+    for img in rel.all_images:
+        location = "/".join((rel.location, img['path']))
+        h = hashify(
+                rel.version,
+                rel.milestone,
+                img['arch'],
+                location,
+                img['variant'],
+                img['subvariant'])
+        output.append(h)
+
+print json.dumps(output)
