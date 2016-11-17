86af7f85b45fc0ca9738f4cfe77fe9752775fb8f
Robert Mayr <robyduck@fedoraproject.org>
2016-11-17 23:58:30 +0100

    RC 1.3 is declared GOLD

--- build.d/globalvar.py
+++ build.d/globalvar.py
@@ -45,8 +45,8 @@ release={
     # Note that atomic values here get overwritten by the twoweek script.
     'pre_cloud_atomic_composedate': '20150915',
     'pre_cloud_AMI_atomic_composedate': '20150915',
-    'RC_gold': '1.2',             # insert the number of the RC version declared GOLD
-    'RC_build': '1.2',            # sometimes releng use the RC build
+    'RC_gold': '1.3',             # insert the number of the RC version declared GOLD
+    'RC_build': '1.3',            # sometimes releng use the RC build
     'RC_pre_gold': '1.1',         # insert the number of the prerelease RC version declared GOLD
     'RC_pre_build': '1',          # sometimes releng use the RC build
 
