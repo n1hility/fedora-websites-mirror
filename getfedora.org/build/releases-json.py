05b9269e6920a9bc2b3151e4e020f261c77f637f
Robert Mayr <robyduck@fedoraproject.org>
2017-07-11 16:32:39 +0200

    update release.json for FMW and drop prereleases

--- getfedora.org/build/releases-json.py
+++ getfedora.org/build/releases-json.py
@@ -15,15 +15,11 @@ def hashify(version, milestone, arch, link, variant, subvariant):
            }
 
 releases_to_report = [
+	  #, fedfind.release.get_release(27)
+      #, fedfind.release.get_release(27, 'Alpha')
       fedfind.release.get_release(26)
-    , fedfind.release.get_release(26, 'Beta')
-    , fedfind.release.get_release(26, 'Alpha')
     , fedfind.release.get_release(25)
-    , fedfind.release.get_release(25, 'Beta')
-    , fedfind.release.get_release(25, 'Alpha')
     , fedfind.release.get_release(24)
-    , fedfind.release.get_release(24, 'Beta')
-    , fedfind.release.get_release(24, 'Alpha')
     ]
 
 for rel in releases_to_report:
