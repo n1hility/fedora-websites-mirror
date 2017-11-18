1d3dd18a8bdcedc5dcba68d9ce04423b09af9da3
Robert Mayr <robyduck@fedoraproject.org>
2017-11-18 23:26:20 +0100

    add Everything images and checksums to all alt arches

--- build.d/globalvar.py
+++ build.d/globalvar.py
@@ -299,6 +299,9 @@ iso_size={
     'x86_64_ev':        '508',   #In MB
     'i386_ev':          '566',   #In MB
     's390x_ev':         '425',   #In MB
+    'aarch64_ev':       '459',   #In MB
+    'ppc64_ev':         '521',   #In MB
+    'ppc64le_ev':       '488',   #In MB
     # Everything prerelease
     'pre_x86_64_ev':        '511',   #In MB
     'pre_i386_ev':          '507',   #In MB
