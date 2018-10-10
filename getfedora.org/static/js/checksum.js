50ed3c5d267c515766076e43c7ea6406e3099ce7
Sinny Kumari <sinny@redhat.com>
2018-10-10 12:07:43 +0530

    Get direct checksum link for aarch64 and ppc64le images as well
    Related - https://pagure.io/fedora-websites/issue/875#comment-535746
    
    Signed-off-by: Sinny Kumari <sinny@redhat.com>

--- getfedora.org/static/js/checksum.js
+++ getfedora.org/static/js/checksum.js
@@ -1,7 +1,7 @@
 var dir_path = '../../static/checksums/';
 var dock_path = 'https://dl.fedoraproject.org/pub/alt/atomic/stable/Fedora-Atomic-' + curr_atomic_id + '-' + atomic_composedate + '.0/Container/x86_64/images/';
-var atom_path = 'https://dl.fedoraproject.org/pub/alt/atomic/stable/Fedora-' + curr_atomic_id + '-updates-' + atomic_composedate + '.0/AtomicHost/x86_64/images/';
-var iso_path = 'https://dl.fedoraproject.org/pub/alt/atomic/stable/Fedora-' + curr_atomic_id + '-updates-' + atomic_composedate + '.0/AtomicHost/x86_64/iso/';
+var atom_path = 'https://dl.fedoraproject.org/pub/alt/atomic/stable/Fedora-' + curr_atomic_id + '-updates-' + atomic_composedate + '.0/AtomicHost/';
+var iso_path = 'https://dl.fedoraproject.org/pub/alt/atomic/stable/Fedora-' + curr_atomic_id + '-updates-' + atomic_composedate + '.0/AtomicHost/';
 
 var checksums = [];
 // Generate dynamic checksum paths
@@ -15,11 +15,17 @@ checksums['Fedora-Server-netinst-aarch64-' + curr_server_id + '-' + RC_server_go
 checksums['Fedora-Server-dvd-aarch64-' + curr_server_id + '-' + RC_server_gold + '.iso'] = dir_path + 'Fedora-Server-' + curr_server_id + '-' + RC_server_gold + '-aarch64-CHECKSUM';
 checksums['Fedora-Server-' + curr_server_id + '-' + RC_server_gold + '.aarch64.raw.xz'] = dir_path + 'Fedora-Server-disk-' + curr_server_id + '-' + RC_server_gold + '-aarch64-CHECKSUM';
 checksums['Fedora-Container-Base-' + curr_id + '-' + RC_gold + '.x86_64.tar.xz'] = dir_path + 'Fedora-Container-' + curr_id + '-' + RC_gold + '-x86_64-CHECKSUM';
-checksums['Fedora-AtomicHost-' + curr_atomic_id + '-' + atomic_composedate + '.0.x86_64.qcow2'] = atom_path + 'Fedora-AtomicHost-' + curr_atomic_id + '-' + atomic_composedate + '.0-x86_64-CHECKSUM';
-checksums['Fedora-AtomicHost-' + curr_atomic_id + '-' + atomic_composedate + '.0.x86_64.raw.xz'] = atom_path + 'Fedora-AtomicHost-' + curr_atomic_id + '-' + atomic_composedate + '.0-x86_64-CHECKSUM';
-checksums['Fedora-AtomicHost-Vagrant-' + curr_atomic_id + '-' + atomic_composedate + '.0.x86_64.vagrant-libvirt.box'] = atom_path + 'Fedora-AtomicHost-' + curr_atomic_id + '-' + atomic_composedate + '.0-x86_64-CHECKSUM';
-checksums['Fedora-AtomicHost-Vagrant-' + curr_atomic_id + '-' + atomic_composedate + '.0.x86_64.vagrant-virtualbox.box'] = atom_path + 'Fedora-AtomicHost-' + curr_atomic_id + '-' + atomic_composedate + '.0-x86_64-CHECKSUM';
-checksums['Fedora-AtomicHost-ostree-x86_64-' + curr_atomic_id + '-' + atomic_composedate + '.0.iso'] = iso_path + 'Fedora-AtomicHost-' + curr_atomic_id + '-' + atomic_composedate + '.0-x86_64-CHECKSUM';
+checksums['Fedora-AtomicHost-' + curr_atomic_id + '-' + atomic_composedate + '.0.aarch64.qcow2'] = atom_path + 'aarch64/images/Fedora-AtomicHost-' + curr_atomic_id + '-' + atomic_composedate + '.0-aarch64-CHECKSUM';
+checksums['Fedora-AtomicHost-' + curr_atomic_id + '-' + atomic_composedate + '.0.ppc64le.qcow2'] = atom_path + 'ppc64le/images/Fedora-AtomicHost-' + curr_atomic_id + '-' + atomic_composedate + '.0-ppc64le-CHECKSUM';
+checksums['Fedora-AtomicHost-' + curr_atomic_id + '-' + atomic_composedate + '.0.x86_64.qcow2'] = atom_path + 'x86_64/images/Fedora-AtomicHost-' + curr_atomic_id + '-' + atomic_composedate + '.0-x86_64-CHECKSUM';
+checksums['Fedora-AtomicHost-' + curr_atomic_id + '-' + atomic_composedate + '.0.aarch64.raw.xz'] = atom_path + 'aarch64/images/Fedora-AtomicHost-' + curr_atomic_id + '-' + atomic_composedate + '.0-aarch64-CHECKSUM';
+checksums['Fedora-AtomicHost-' + curr_atomic_id + '-' + atomic_composedate + '.0.ppc64le.raw.xz'] = atom_path + 'ppc64le/images/Fedora-AtomicHost-' + curr_atomic_id + '-' + atomic_composedate + '.0-ppc64le-CHECKSUM';
+checksums['Fedora-AtomicHost-' + curr_atomic_id + '-' + atomic_composedate + '.0.x86_64.raw.xz'] = atom_path + 'x86_64/images/Fedora-AtomicHost-' + curr_atomic_id + '-' + atomic_composedate + '.0-x86_64-CHECKSUM';
+checksums['Fedora-AtomicHost-Vagrant-' + curr_atomic_id + '-' + atomic_composedate + '.0.x86_64.vagrant-libvirt.box'] = atom_path + 'x86_64/images/Fedora-AtomicHost-' + curr_atomic_id + '-' + atomic_composedate + '.0-x86_64-CHECKSUM';
+checksums['Fedora-AtomicHost-Vagrant-' + curr_atomic_id + '-' + atomic_composedate + '.0.x86_64.vagrant-virtualbox.box'] = atom_path + 'x86_64/images/Fedora-AtomicHost-' + curr_atomic_id + '-' + atomic_composedate + '.0-x86_64-CHECKSUM';
+checksums['Fedora-AtomicHost-ostree-aarch64-' + curr_atomic_id + '-' + atomic_composedate + '.0.iso'] = iso_path + 'aarch64/iso/Fedora-AtomicHost-' + curr_atomic_id + '-' + atomic_composedate + '.0-aarch64-CHECKSUM';
+checksums['Fedora-AtomicHost-ostree-ppc64le-' + curr_atomic_id + '-' + atomic_composedate + '.0.iso'] = iso_path + 'ppc64le/iso/Fedora-AtomicHost-' + curr_atomic_id + '-' + atomic_composedate + '.0-ppc64le-CHECKSUM';
+checksums['Fedora-AtomicHost-ostree-x86_64-' + curr_atomic_id + '-' + atomic_composedate + '.0.iso'] = iso_path + 'x86_64/iso/Fedora-AtomicHost-' + curr_atomic_id + '-' + atomic_composedate + '.0-x86_64-CHECKSUM';
 checksums['Fedora-AtomicWorkstation-ostree-x86_64-' + curr_id + '-' + RC_gold + '.iso'] = dir_path + 'Fedora-AtomicWorkstation-' + curr_id + '-' + RC_gold + '-x86_64-CHECKSUM';
 
 
