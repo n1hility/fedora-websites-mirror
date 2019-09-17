21b5d188235688e55e78d1dc99bfacbb98218d58
Paul W. Frields <stickster@gmail.com>
2019-09-17 12:18:59 -0400

    Fixes for 31 Beta

--- build.d/globalvar.py
+++ build.d/globalvar.py
@@ -7,9 +7,9 @@ release={
     'next_id':     '31',
     'curr_name':   '',
     'next_name':   '',
-    'curr_state':  '',           # either 'Alpha', 'Beta' or '' (i.e empty)
-    'curr_arm_state':  '',       # either 'Alpha', 'Beta' or '' (i.e empty)
-    'curr_alt_state':  '',       # either 'Alpha', 'Beta' or '' (i.e empty)
+    'curr_state':  'Beta',           # either 'Alpha', 'Beta' or '' (i.e empty)
+    'curr_arm_state':  'Beta',       # either 'Alpha', 'Beta' or '' (i.e empty)
+    'curr_alt_state':  'Beta',       # either 'Alpha', 'Beta' or '' (i.e empty)
     'curr_cloud_state':  '',     # either 'Alpha', 'Beta' or '' (i.e empty)
     'curr_cloud_AMI_state':  '', # either 'Alpha', 'Beta' or '' (i.e empty)
     'curr_server_state':  '',    # either 'Alpha', 'Beta' or '' (i.e empty)
@@ -60,12 +60,12 @@ release={
     'RC_gold_aarch64': '1.2',     # RC Gold for aarch64
     'RC_gold_ppc64': '1.2',       # RC Gold for ppc64
     'RC_gold_s390x': '1.2',       # RC Gold for s390x
-    'RC_pre_gold': '1.8',         # insert the number of the prerelease RC version declared GOLD
+    'RC_pre_gold': '1.1',         # insert the number of the prerelease RC version declared GOLD
     'RC_pre_build': '2',        # sometimes releng use the RC build
-    'RC_pre_gold_aarch64': '1.8', # Pre RC Gold for aarch64
-    'RC_pre_gold_ppc64': '1.8',   # Pre RC Gold for ppc64
-    'RC_pre_gold_ppc64le': '1.8', # Pre RC Gold for ppc64le
-    'RC_pre_gold_s390x': '1.8',   # Pre RC Gold for s390x
+    'RC_pre_gold_aarch64': '1.1', # Pre RC Gold for aarch64
+    'RC_pre_gold_ppc64': '1.1',   # Pre RC Gold for ppc64
+    'RC_pre_gold_ppc64le': '1.1', # Pre RC Gold for ppc64le
+    'RC_pre_gold_s390x': '1.1',   # Pre RC Gold for s390x
 
     'atomic_qcow2_cloud_url': 'FIXME',
     'atomic_raw_cloud_url': 'FIXME',
