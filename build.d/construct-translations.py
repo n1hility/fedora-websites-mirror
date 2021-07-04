fae17f0ef550a7930dd00ea355bf4f0f5e718cfc
Francois Andrieu <darknao@fedoraproject.org>
2021-07-04 18:08:40 +0200

    redesign language menu and drop jquery.uls

--- build.d/construct-translations.py
+++ build.d/construct-translations.py
@@ -13,41 +13,41 @@ language_map.readfp(open(sys.argv[1]))
 
 banned = language_map.get('Banned', 'lang_code')
 
+languages = [('en', 'English')]
 with open(sys.argv[2], 'r') as linguas: # Linguas is where available languages are stored
-    with open(sys.argv[3], 'w') as output: # Output is a genshi template
-        output.write('''<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-		<html xmlns="http://www.w3.org/1999/xhtml"
-		  xmlns:py="http://genshi.edgewall.org/"
-		  xmlns:xi="http://www.w3.org/2001/XInclude"
-		  py:strip="">
+    for lang in linguas:
+        lang = lang.strip()
+        if lang and not lang.startswith('#') and not lang in banned:
+            languages.append((lang, langtable.language_name(languageId=lang).encode('utf-8')))
+languages = sorted(languages)
 
-		<!-- This file is generated, don't edit here -->
+with open(sys.argv[3], 'w') as output: # Output is a genshi template
+    output.write('''<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+	<html xmlns="http://www.w3.org/1999/xhtml"
+	  xmlns:py="http://genshi.edgewall.org/"
+	  xmlns:xi="http://www.w3.org/2001/XInclude"
+	  py:strip="">
 
-		''')
-        output.write('''
-		<script type="text/javascript">
-		/* This list is generated, don't edit here */
-		$(function() {
-			$( '.uls-trigger' ).uls( {
-				onSelect : function( language ) {
-					var languageName = $.uls.data.getAutonym( language );
-					$( '.uls-trigger' ).text( languageName );
-                    $('#selectedLang').val(language);
-                    $('#langSelect').trigger('submit');
-				},
-				languages: { 'en' : 'English' ''')
-        try:
-            for lang in linguas:
-                lang = lang.strip()
-                if lang and not lang.startswith('#') and not lang in banned:
-                    #output.write('    <option value="' + lang + '" py:attrs="{\'selected\': lang == \'' + lang + '\' and \'selected\' or None}">' + language_map.get('Languages',lang) + '</option>\n')
-                    #output.write(", '"+lang+"': '"+language_map.get('Languages',lang)+"'")
-                    output.write(", '"+lang+"': '"+langtable.language_name(languageId=lang).encode('utf-8')+"'")
-        finally:
-            linguas.close()
-        output.write("""}
-			} );
-		} );
-		</script>
-		""")
-        output.write('</html>')
+	<!-- This file is generated, don't edit here -->
+
+	''')
+
+    output.write('''
+    <div class="dropup language-header">
+      <span class="fa fa-language fa-lg"></span>
+      <a class="dropdown-toggle" href="#" role="button" id="langSelect" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Change Language
+      </a>
+      <div class="dropdown-menu dropdown-menu-right language-menu" aria-labelledby="langSelect">
+        <ul class="list-unstyled text-center" id="languagelist">
+    ''')
+
+    for l_code, l_name in languages:
+        output.write('<li><a href="/'+ l_code +'/">'+ l_name +'</a></li>')
+
+    output.write("""
+        </ul>
+      </div>
+    </div>
+	""")
+    output.write('</html>')
