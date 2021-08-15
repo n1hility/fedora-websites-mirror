bc46653a53330822500103b59f96f812bcaa494c
Francois Andrieu <darknao@fedoraproject.org>
2021-08-16 00:21:27 +0200

    add language button to fedoracommunity

--- fedoracommunity.org/static/js/init.js
+++ fedoracommunity.org/static/js/init.js
@@ -1,4 +1,8 @@
 // load widgets on pageload 
 $(function() { 
 	// spaceholder
+        $( "#langSelect" ).click(function() {
+           $(".language-menu").toggle();
+           return false;
+        });
 });
